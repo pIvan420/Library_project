529b95e5a47ca26945f862578a190c3e37e43a6f
pIvan420 <iv-torsunow@yandex.ru>
2023-08-04 01:55:30 +0300

    Внес базовую конфигурацию проекта.
    Подключил все нужные фреймворки в pom.xml,
    создал необходимые директории, настроил файлы конфигурации,
    подключил БД и удаленный сервер

---
+++ src/main/java/ru/pivan/config/SpringConfig.java
@@ -0,0 +1,78 @@
+package ru.pivan.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("ru.pivan")
+@EnableWebMvc //todo вспомнить, чо делает эта аннотация
+@PropertySource("classpath:database.properties")
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext; // контейнер spring'а, где хранятся бины
+
+    private final Environment environment; // позволяет безопасно получить данные из database.properties
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
+        this.applicationContext = applicationContext;
+        this.environment = environment;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){ //дефолтная конфигурация, как и где получать представления
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry){ //делает Thymeleaf основным шаблонизатором
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource(){ //подключаю БД
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("db.driver");
+        dataSource.setUrl("db.url");
+        dataSource.setUsername("db.login");
+        dataSource.setPassword("db.password");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){ //подключаю драйвер для работы с БД
+        return new JdbcTemplate(dataSource());
+    }
+
+}
